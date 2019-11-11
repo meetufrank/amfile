abc237ee47f36341f4d2efcd058cba14b2f664e5
zhaoqilong <test@qq.com>
2019-11-11 16:08:56 +0800

    恢复聊天js html

--- public/static/laychat/common/layui/layui.js
+++ public/static/laychat/common/layui/layui.js
@@ -337,18 +337,17 @@ Lay.fn.device = function(key){
   };
 
   var result = {
-      os: function(){ //底层操作系统
-          if(/windows/.test(agent)){
-              return 'windows';
-          } else if(/linux/.test(agent)){
-              return 'linux';
-          }else if(/iphone|ipod|ipad|ios/.test(agent)){
-              return 'ios';
-          }
-          else if(/mac/.test(agent)){
-              return 'mac';
-          }
-      }()
+    os: function(){ //底层操作系统
+      if(/windows/.test(agent)){
+        return 'windows';
+      } else if(/linux/.test(agent)){
+        return 'linux';
+      } else if(/mac/.test(agent)){
+        return 'mac';
+      } else if(/iphone|ipod|ipad|ios/.test(agent)){
+        return 'ios';
+      }
+    }()
     ,ie: function(){ //ie版本
       return (!!win.ActiveXObject || "ActiveXObject" in win) ? (
         (agent.match(/msie\s(\d+)/) || [])[1] || '11' //由于ie11并没有msie的标识
